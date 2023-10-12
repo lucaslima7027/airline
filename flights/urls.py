ad6dbbd8de5d3f191961384f218d426208bfb0c9
Lucas Lima <limalucas7027@gmail.com>
2023-10-12 19:28:09 -0300

    Flight detail page was created
    Route created on url.py
    flight function was created this function requires the flight id to render the detail page
    Added a base template layout.html and extended it to other templates

--- flights/urls.py
+++ flights/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:flight_id>", views.flight, name="flight")
 ]
--- flights/views.py
+++ flights/views.py
@@ -7,3 +7,10 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    flightRequested = Flight.objects.get(id=flight_id)
+
+    return render(request, "flights/flight.html",{
+        "flightRequested": flightRequested
+    })
