c6234f7e392d647710de2a2af3285da9611858be
Lucas Lima <limalucas7027@gmail.com>
2023-10-16 22:19:23 -0300

    Added a form to include a passenger in a flight
    form created in flight.html
    The form shows all non passenger and lets the user choose from a select field to add that passenger to the flight

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 
@@ -13,5 +15,14 @@ def flight(request, flight_id):
 
     return render(request, "flights/flight.html",{
         "flightRequested": flightRequested,
-        "passengers": flightRequested.passengers.all()
+        "passengers": flightRequested.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flightRequested).all
     })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=[flight_id]))
+
